eaa36c669753d7ff653f0ae38930a0eb36d2634c
Taran098 <sin12506@sheridancollege.ca>
2023-09-20 00:54:51 -0400

    Only AJAX left

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $username = "sin12506_Taran";
 $password = "Taranjeet@09";
 $dbname = " sin12506_Portfolio_Feedback";
 
-$con = mysqli_connect("dev.fast.sheridanc.on.ca", "sin12506_Taran", "Taranjeet@09", "sin12506_Portfolio_Feedback");
+$conn = mysqli_connect("dev.fast.sheridanc.on.ca", "sin12506_TaranFeedback", "Userreview$09", "sin12506_Portfolio_Feedback");
 
 
 if ($conn->connect_error) {
@@ -13,14 +13,14 @@ if ($conn->connect_error) {
 }
 
 // if form submitted
-if($_SERVER["REQUEST_METHOD"] = "POST") {
+if($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $feedback = $_POST["feedback"];
 
 
     // sql database 
-    $sql = "INSERT INTO UserReview (name, email, feedback) VALUES ('$name', '$email', '$feedback')";
+    $sql = "INSERT INTO USERREVIEW(name, email, feedback) VALUES ('$name', '$email', '$feedback')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Feedback submitted successfully!";
